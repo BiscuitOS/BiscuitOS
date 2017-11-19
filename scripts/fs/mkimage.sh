77c0e5a93cbca6a8431fba9ffaabc06a9b938c91
BuddyZhang1 <buddy.zhang@aliyun.com>
2017-11-19 16:37:28 +0800

    minix: create a minixfs via mkfs.minix

--- scripts/fs/mkimage.sh
+++ scripts/fs/mkimage.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
-
+###############################################
+#
+# build a minix-fs image
+#
+#  Mount:
+#   mount -t minix minix.img -o loop mount_dir
+###############################################
 ROOT=`pwd`
 
-IMAGE=${ROOT}/hdc.img
+IMAGE=${ROOT}/minix.img
 ROOTFS=${ROOT}/rootfs.img
-empty_size=2     # MB
+mbr_size=1       # MB
 rootfs_size=20   # MB
-
-# Create boot and root partition
-dd bs=1M count=${empty_size} if=/dev/zero of=${IMAGE}
 
 # Create Minix filesystem
 dd bs=1M count=${rootfs_size} if=/dev/zero of=${ROOTFS}
 mkfs.minix ${ROOTFS}
 
-# append rootfs to boot
-dd bs=1M seek=${empty_size} conv=notrunc oflag=append \
-         if=${ROOTFS} of=${IMAGE}
+# Create MBR partition
+dd bs=1M count=${mbr_size} if=/dev/zero of=${IMAGE}
 
-rm -rf ${ROOTFS}
-
-# Add partition table
+# Create MBR partition table
 cat <<EOF | fdisk "${IMAGE}"
-o
 n
 p
 1
-$((empty_size*1024*2))
-$((rootfs_size*1024*2))
-t
-c
+
+
 w
 EOF
+
+# Append MINIX fs into Rootfs
+dd bs=512 seek=1 if=${ROOTFS} of=${IMAGE}
+rm -rf ${ROOTFS}
 
 sync
