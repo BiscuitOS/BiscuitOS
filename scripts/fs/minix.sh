1c7b2760f48ca5f6d9075b7b89b8408ba1f4f474
BuddyZhang1 <buddy.d.zhang@gmail.com>
2017-11-28 23:34:29 +0800

    minix: build a binary-image for minixfs

--- scripts/fs/minix.sh
+++ scripts/fs/minix.sh
@@ -2,7 +2,25 @@
 
 #
 # Create minix rootfs
-# (C) 2017.11 <buddy.zhang@aliyun.com>
+#  MINIX was written from scratch by Andrew S. Tanenbaum in the 1980s, as 
+#  a Unix-like operating system whose source code could be used freely in 
+#  education. The MINIX file system was designed for use with MINIX. it 
+#  copies the basic structure of the Unix File System but avoids any complex
+#  features in the interest of keeping the source code clean, clear and 
+#  simple, to meet the overall goal of MINIX to be a useful teaching aid.
+#
+#  When Linus Torvalds first started writing his Linux operating system
+#  kernel (1991), he was working on a machine running MINIX, and adopted
+#  its file system layout. This soon proved problematic, since MINIX 
+#  restricted filename lengths to fourteen characters (thirty in later 
+#  versions), it limited partitions to 64 megabytes, and the file system 
+#  was designed for teaching purposes, not performance. The Extended file 
+#  system (ext; April 1992) was developed to replace MINIX's, but it was 
+#  only with the second version of this, ext2, that Linux obtained a 
+#  commercial-grade file system. As of 1994, the MINIX file system was 
+#  "scarcely in use" among Linux users.
+#
+#  (C) 2017.11 <buddy.zhang@aliyun.com>
 #
 
 # $1: Root dirent
@@ -25,6 +43,12 @@ DOWNLOAD_SITE=$3/$2.$5
 
 HOST_BIN=${ROOT}/output/host/bin/mkfs.minix
 
+####
+# Bootable image
+#
+MBR_sect=2048       # sectors (Support EFI BIOS, Reserved 1M)
+ROOTFS_sect=81920   # sectors
+
 # Prepare staging
 if [ ! -d ${STAGING_DIR} ]; then
   mkdir -p ${STAGING_DIR}
@@ -42,24 +66,42 @@ fi
 ##
 # Build minix from mkfs.minix
 if [ $6 == "host_build" -a ! -f ${IMAGE_DIR}/${IMAGE_NAME}-$4.img ]; then
-  dd bs=1M count=10 if=/dev/zero of=${IMAGE_DIR}/${IMAGE_NAME}-$4.img
-  # Create MBR partition table
+  # Create MBR partition
+  #  With the death of the legacy BIOS and its replancement with EFI BIOS
+  #  a specia I boot partitions needed to allow EFI system to boot in EFI
+  #  mode. Starting the first partition at sector 2048 leaves 1Mb for the
+  #  EFI code. Modern partiting tools do this anyway and fdisk has been
+  #  updated to follow suit.
+  #
+  dd if=/dev/zero bs=512 count=${MBR_sect} \
+     of=${IMAGE_DIR}/mbr.img > /dev/null 2>&1
+
+  # Create MINIXFS partition
+  #  Specify rootfs append MBR partition.
+  dd if=/dev/zero bs=512 count=${ROOTFS_sect} \
+     of=${IMAGE_DIR}/minixfs.img > /dev/null 2>&1
+  mkfs.minix -1 ${IMAGE_DIR}/minixfs.img > /dev/null 2>&1
+
+  # Append minixfs into mbr
+  dd if=${IMAGE_DIR}/minixfs.img conv=notrunc oflag=append bs=512  \
+     seek=${MBR_sect} of=${IMAGE_DIR}/mbr.img > /dev/null 2>&1
+  rm -rf ${IMAGE_DIR}/minixfs.img
+  mv ${IMAGE_DIR}/mbr.img ${IMAGE_DIR}/${IMAGE_NAME}-$4.img
+  sync
+
+  # Add partition table
 cat <<EOF | fdisk "${IMAGE_DIR}/${IMAGE_NAME}-$4.img"
 n
 p
 1
+${MBR_sect}
 
-
+t
+81
 w
 EOF
-  # Create MINIX partition
-  dd bs=1M count=40 if=/dev/zero of=${IMAGE_DIR}/minix.img
-  mkfs.minix ${IMAGE_DIR}/minix.img
-  # Append MINIX fs into ROOTFS
-  dd bs=512 seek=1 if=${IMAGE_DIR}/minix.img \
-                   of=${IMAGE_DIR}/${IMAGE_NAME}-$4.img
-  rm -rf of=${IMAGE_DIR}/minix.img
   sync
+
 else
   # Download or Reload Rootfs
   if [ ! -f ${DL_DIR}/${ROOT_BZ2} ]; then
@@ -82,7 +124,14 @@ if [ -d ${IMAGE_DIR}/.rootfs ]; then
 fi
 echo -e "\e[1;31m  This procedure need root permission. \e[0m"
 mkdir -p ${IMAGE_DIR}/.rootfs
-sudo losetup -o 512 /dev/loop3 ${IMAGE_DIR}/${IMAGE_NAME}-$4.img
+
+##
+# Mount image into /dev/loopx
+if [ $6 == "host_build" ]; then
+  sudo losetup -o 1048576 /dev/loop3 ${IMAGE_DIR}/${IMAGE_NAME}-$4.img
+else
+  sudo losetup -o 512 /dev/loop3 ${IMAGE_DIR}/${IMAGE_NAME}-$4.img
+fi
 sudo mount /dev/loop3 ${IMAGE_DIR}/.rootfs
 sudo cp -rfa ${IMAGE_DIR}/rootfs/* ${IMAGE_DIR}/.rootfs
 sync
--- scripts/fs/mkimage.sh
+++ scripts/fs/mkimage.sh
@@ -4,36 +4,45 @@
 # build a minix-fs image
 #
 #  Mount:
-#   mount -t minix minix.img -o loop mount_dir
+#   losetup -o 1048576 /dev/loop3 mbr.img
+#   mount -t minix mbr.img mount_dir
 ###############################################
 ROOT=`pwd`
+OUTDIR=${ROOT}
 
-IMAGE=${ROOT}/mbr.img
-ROOTFS=${ROOT}/minix.img
-mbr_size=16      # MB
-rootfs_size=20   # MB
-
-# Create Minix filesystem
-dd bs=1M count=${rootfs_size} if=/dev/zero of=${ROOTFS}
-mkfs.minix ${ROOTFS}
+# Partition size in sector (512 Bytes)
+MBR_sect=2048            # sectors (Support EFI BIOS, Reserved 1M)
+ROOTFS_sect=81920        # sectors
 
 # Create MBR partition
-dd bs=1M count=${mbr_size} if=/dev/zero of=${IMAGE}
+#  With the death of the legacy BIOS and its replancement with EFI BIOS
+#  a specia I boot partitions needed to allow EFI system to boot in EFI
+#  mode. Starting the first partition at sector 2048 leaves 1Mb for the
+#  EFI code. Modern partiting tools do this anyway and fdisk has been
+#  updated to follow suit.
+#
+dd if=/dev/zero bs=512 count=${MBR_sect} of=${OUTDIR}/mbr.img
+
+# Create MINIXFS partition
+#  Specify rootfs append MBR partition.
+dd if=/dev/zero bs=512 count=${ROOTFS_sect} of=${OUTDIR}/minixfs.img
+mkfs.minix -1 ${OUTDIR}/minixfs.img
+
+# Append minixfs into mbr
+dd if=${OUTDIR}/minixfs.img conv=notrunc oflag=append bs=512  \
+        seek=${MBR_sect} of=${OUTDIR}/mbr.img
+rm -rf ${OUTDIR}/minixfs.img
 sync
 
-# Create MBR partition table
-cat <<EOF | fdisk "${IMAGE}"
+# Add partition table
+cat <<EOF | fdisk "${OUTDIR}/mbr.img"
 n
 p
 1
+${MBR_sect}
 
-
+t
+81
 w
 EOF
-
-sync
-# Append MINIX fs into Rootfs
-dd bs=512 seek=1 if=${ROOTFS} of=${IMAGE}
-rm -rf ${ROOTFS}
-
 sync
