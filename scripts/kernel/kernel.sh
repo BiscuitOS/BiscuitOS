6fd89ab24807b277c0b6d9f0116c86fb5537daae
BuddyZhang1 <buddy.zhang@aliyun.com>
2018-08-20 11:37:45 +0800

    Linux 1.0.1.2: Debug version for Register

--- scripts/kernel/kernel.sh
+++ scripts/kernel/kernel.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #
-# This scripts is used to download and establish kernel 
+# This scripts is used to download and establish kernel of BiscuitOS
+# Don't edit, if you want please mail to me :-)
 #
-# (C) 2018.07.23 BiscuitOS <buddy.zhang@aliyun.com>
+# (C) 2018.07.23 BuddyZhang1 <buddy.zhang@aliyun.com>
+# (C) 2018.08.20 BiscuitOS <buddy.biscuitos@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -17,43 +19,13 @@ github=$2
 kernel_version=$1
 kernel_magic=$4
 fs_magic=$5
+tag_version=$6
 
 ###
 # Don't edit
 KERN_SOURCE=${srctree}/dl/kernel
 TARGET=${srctree}/kernel/linux_${kernel_version}
 PATCH_DIR=${srctree}/target/kernel/patch/linux_${kernel_version}
-
-### Kernel version
-KVersion=(
-"0.11"
-"0.12"
-"0.95.1"
-"0.95.3"
-"0.95a"
-"0.96.1"
-"0.97.1"
-"0.98.1"
-"0.99.1"
-"1.0.1"
-"1.0.1.1"
-"1.0.1.2"
-)
-
-TagVersion=(
-"v0.11.3"
-"v0.12.1"
-"v0.95.1"
-"v0.95.3"
-"v0.95a"
-"v0.96.1"
-"v0.97.1"
-"v0.98.1"
-"v0.99.1"
-"v1.0.1"
-"v1.0.1.1"
-"v1.0.1.1"
-)
 
 download_kernel()
 {
@@ -70,34 +42,26 @@ establish_kernel()
         cp -rf ${KERN_SOURCE} ${TARGET}
     fi
     cd ${TARGET}
-    ## Check and change source version
-    j=0
-    for i in ${KVersion[@]}; do
-        if [ $i = ${kernel_version} ]; then
-            ## Establish kernel
-            if [ $1 = "establish" ]; then
-                if [ -d ${TARGET}/tools/means ]; then
-                    rm -rf ${TARGET}/tools/means
-                fi
-                git reset --hard ${TagVersion[$j]} > /dev/null 2>&1
-                git am ${PATCH_DIR}/*.patch
-            fi
-            if [ ${kernel_magic} -gt 9 ]; then
-                if [ ${fs_magic} -eq 0 ]; then
-                    make linux_minix_defconfig
-                fi
-                if [ ${fs_magic} -eq 1 ]; then
-                    make linux_ext2_defconfig
-                fi
-                if [ ${fs_magic} -eq 2 ]; then
-                    make linux_msdos_defconfig
-                fi
-            else
-                make defconfig
-            fi
+    if [ $1 = "establish" ]; then
+        if [ -d ${TARGET}/tools/means ]; then
+            rm -rf ${TARGET}/tools/means
         fi
-    j=`expr $j + 1`
-    done
+        git reset --hard ${tag_version} > /dev/null 2>&1
+        git am ${PATCH_DIR}/*.patch > /dev/null 2>&1
+    fi
+    if [ ${kernel_magic} -gt 9 ]; then
+        if [ ${fs_magic} -eq 0 ]; then
+            make linux_minix_defconfig
+        fi
+        if [ ${fs_magic} -eq 1 ]; then
+            make linux_ext2_defconfig
+        fi
+        if [ ${fs_magic} -eq 2 ]; then
+            make linux_msdos_defconfig
+        fi
+    else
+        make defconfig
+    fi
     cd - > /dev/null 2>&1
 }
 
